9a9e97f06cb23e6f79a84d2cf0131f7442fcb242
Nuno Moutinho <nunomoutinho@ua.pt>
2025-03-15 14:03:00 +0000

    MyDLL.c(init.c)

--- MyDLL.c
+++ MyDLL.c
@@ -1,4 +1,18 @@
-void MyDLLInit(void){
+#include<stdio.h>
+#include<stdlib.h>
+#include"MyDLL.h"
+
+void MyDLLInit(DoublyLinkedlist *dll, size_t max, size_t elememt_size){
+	if(max == 0 || element_size == 0){
+		printf("ERROR(Invalid value)");
+		return
+	}
+	
+	dll -> max = max;
+	dll -> elememt_size = elememt_size;
+	dll -> head = NULL;
+	dll -> tail = NULL;
+	dll -> current_size = 0;
 }
 
 void MyDLLInsert(void){
