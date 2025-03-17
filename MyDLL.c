a9de6a839e1b603a1879307a5608b2c0645f90ef
Nuno Moutinho <nunomoutinho@ua.pt>
2025-03-17 15:48:57 +0000

    Funçoes FindNext e FindPrevious

--- MyDLL.c
+++ MyDLL.c
@@ -39,4 +39,44 @@ unsigned char* MyDLLFind(DoublyLinkedList *dll, uint16_t key) {
     return NULL;    
 } 
 
-void MyDLLFind{Next,Previous}
+unsigned char* MyDLLFindNext(DoublyLinkedList* dll, uint16_t key){
+	
+	if(dll->current_size == 0){ // Verificar se a lista está vazia
+		printf("Error! Empty list \n");
+		return NULL;
+	}
+	Node *currentNode = dll->head;	
+	while (currentNode != NULL && currentNode->key!= key) { // Percorrer a lista
+        currentNode = currentNode->next; // Avançar para o próximo nó
+	}
+	if(currentNode->key == key) { // Verificar se a chave foi encontrada
+		if(currentNode->next == NULL){ // Verificar se é o último nó
+			printf("Error! No next node\n");
+			return NULL;
+		}
+		return currentNode->next->data; // Retornar os dados
+	}   
+	printf("Error! Key not found\n");
+	return NULL;
+}
+
+unsigned char* MyDLLFindPrev(DoublyLinkedList* dll, uint16_t key){
+
+	if(dll->current_size == 0){
+		printf("Error! Empty list \n");
+		return NULL;
+	}
+	Node *currentNode = dll->head;	
+	while (currentNode != NULL && currentNode->key!= key) { // Percorrer a lista
+        currentNode = currentNode->next; // Avançar para o próximo nó
+	}
+	if(currentNode->key == key) { // Verificar se a chave foi encontrada
+		if(currentNode->prev == NULL){ // Verificar se é o último nó
+			printf("Error! No previous node\n");
+			return NULL;
+		}
+		return currentNode->prev->data; // Retornar os dados
+	}   
+	printf("Error! Key not found\n");
+	return NULL;
+}
